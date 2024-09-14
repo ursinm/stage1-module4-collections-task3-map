978d1fe99987c930ec0ebc40ad7eb25593ef8797
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:46:07 +0300

    Update FunctionValueFinderTest.java

--- src/test/java/com/epam/mjc/collections/map/FunctionValueFinderTest.java
+++ src/test/java/com/epam/mjc/collections/map/FunctionValueFinderTest.java
@@ -10,31 +10,53 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FunctionValueFinderTest {
+
     public static Stream<Arguments> testCases() {
         return Stream.of(
+                // Пустой список, не может содержать значение
                 Arguments.of("EmptySourceList",
                         List.of(),
                         52,
                         false),
 
-                Arguments.of("NotPresent",
+                // Значение не присутствует среди результатов функции
+                Arguments.of("ValueNotPresent",
                         List.of(2, -6, 19, 55, 11),
                         52,
                         false),
 
-                Arguments.of("Present",
+                // Значение присутствует среди результатов функции
+                Arguments.of("ValuePresent",
                         List.of(2, -6, 10, 19, 55, 11),
                         52,
+                        true),
+
+                // Проверка нуля
+                Arguments.of("ZeroValue",
+                        List.of(0, 1, -1),
+                        2,
+                        true),
+
+                // Проверка отрицательных значений
+                Arguments.of("NegativeValuePresent",
+                        List.of(-10, -20, -30),
+                        -48,
+                        true),
+
+                // Проверка большого диапазона
+                Arguments.of("LargeValuePresent",
+                        List.of(1000000, 2000000, 3000000),
+                        5000002,
                         true)
         );
     }
 
     @ParameterizedTest(name = "isFunctionValuePresent_{0}_Test")
     @MethodSource(value = "testCases")
-    void isFunctionValuePresent(String name,
-                                List<Integer> sourceList,
-                                int requiredValue,
-                                boolean isValuePresent) {
+    void isFunctionValuePresentTest(String name,
+                                    List<Integer> sourceList,
+                                    int requiredValue,
+                                    boolean isValuePresent) {
 
         FunctionValueFinder finder = new FunctionValueFinder();
         assertEquals(isValuePresent, finder.isFunctionValuePresent(sourceList, requiredValue));
