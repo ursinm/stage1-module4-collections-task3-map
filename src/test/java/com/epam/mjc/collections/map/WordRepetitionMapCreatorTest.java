9dacca69438b7279f32fa2a9f3fde1a02b81de79
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:48:23 +0300

    Update WordRepetitionMapCreatorTest.java

--- src/test/java/com/epam/mjc/collections/map/WordRepetitionMapCreatorTest.java
+++ src/test/java/com/epam/mjc/collections/map/WordRepetitionMapCreatorTest.java
@@ -13,10 +13,12 @@ public class WordRepetitionMapCreatorTest {
 
     public static Stream<Arguments> testCases() {
         return Stream.of(
+                // Пустая строка
                 Arguments.of("FromEmptySentence",
                         "",
                         Map.of()),
 
+                // Строка без повторений
                 Arguments.of("FromSentenceWithoutRepetition",
                         "Java Map can store pairs of keys and values.",
                         Map.of("java", 1,
@@ -29,6 +31,7 @@ public class WordRepetitionMapCreatorTest {
                                 "and", 1,
                                 "values", 1)),
 
+                // Строка с повторениями слов, разными регистрами и знаками препинания
                 Arguments.of("FromSentenceWithRepetition",
                         "hello word in lOwEr cAsE, HELLO WORD in UpPeR CaSe.",
                         Map.of("hello", 2,
@@ -36,18 +39,34 @@ public class WordRepetitionMapCreatorTest {
                                 "in", 2,
                                 "lower", 1,
                                 "case", 2,
-                                "upper", 1))
+                                "upper", 1)),
+
+                // Строка с пробелами и знаками препинания
+                Arguments.of("FromSentenceWithSpacesAndPunctuation",
+                        " This,  this! this? ",
+                        Map.of("this", 3)),
+
+                // Строка с одним словом
+                Arguments.of("SingleWordSentence",
+                        "Test",
+                        Map.of("test", 1)),
+
+                // Строка, содержащая только пробелы
+                Arguments.of("OnlySpaces",
+                        "     ",
+                        Map.of())
         );
     }
 
     @ParameterizedTest(name = "createWordRepetitionMap_{0}_Test")
     @MethodSource(value = "testCases")
-    void createWordRepetitionMap(String name,
-                                 String sentence,
-                                 Map<String, Integer> expectedWordRepetitionMap) {
+    void createWordRepetitionMapTest(String name,
+                                     String sentence,
+                                     Map<String, Integer> expectedWordRepetitionMap) {
 
         WordRepetitionMapCreator wordRepetitionMapCreator = new WordRepetitionMapCreator();
         Map<String, Integer> actualWordRepetitionMap = wordRepetitionMapCreator.createWordRepetitionMap(sentence);
         assertEquals(expectedWordRepetitionMap, actualWordRepetitionMap);
     }
 }
+
