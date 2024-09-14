8db777b104180fa5f98596cfa9772b3bdd438b56
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:44:14 +0300

    Update KeyValueSwapper.java

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
+
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swappedMap = new HashMap<>();
+        
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            String value = entry.getValue();
+            Integer key = entry.getKey();
+            
+            // Если уже есть такое значение, сохраняем минимальный ключ
+            if (!swappedMap.containsKey(value) || key < swappedMap.get(value)) {
+                swappedMap.put(value, key);
+            }
+        }
+        
+        return swappedMap;
     }
 }
