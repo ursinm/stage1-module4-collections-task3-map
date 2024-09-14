750eff02ca004b793d270d3896a83948bb5d1441
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:43:48 +0300

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
+
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        
+        // Убираем пунктуацию и разбиваем строку на слова
+        String[] words = sentence.toLowerCase().replaceAll("[^a-zA-Z ]", "").split("\\s+");
+        
+        // Считаем количество вхождений каждого слова
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+            }
+        }
+        
+        return wordCountMap;
     }
 }
